440a3ef3611834d463ccf2129b9c346f8bc43b4f
Grey <Grey@Grey-PC>
2015-04-03 23:47:13 +0800

    normal

--- src/crawler/JD.py
+++ src/crawler/JD.py
@@ -13,8 +13,14 @@ def crawl(search_data):
     
     browser = webdriver.Chrome()
     browser.get('http://search.jd.com/Search?keyword=' + search_data)
-
+    
+#     browser.find_element_by_class_name('jumpto').click()
+    
     element = browser.find_element_by_xpath('/html/body[@class=\'root61\']/div[@class=\'w main\']/div[@class=\'right-extra\']/div[@class=\'m clearfix\']/div[@class=\'pagin fr\']/span[@class=\'page-skip\']/em')
+    
+    
+    
+    
     
 #     print repr(element.text)
 #     print element.text[3:-7]
@@ -26,6 +32,11 @@ def crawl(search_data):
     for i in range(num_page):
         browser.get('http://search.jd.com/Search?keyword=' + search_data + '&psort=4' + '&page=' + str(2*i+1))
         
+        browser.find_element_by_class_name('jumpto').click()
+    
+        element = browser.find_element_by_xpath('/html/body[@class=\'root61\']/div[@class=\'w main\']/div[@class=\'right-extra\']/div[@class=\'m clearfix\']/div[@class=\'pagin fr\']/span[@class=\'page-skip\']/em')
+    
+        
 
         element = browser.find_element_by_xpath('/html/body[@class=\'root61\']')
         element = element.find_element_by_xpath('./div[@class=\'w main\']')
@@ -35,41 +46,45 @@ def crawl(search_data):
         
         list_item = element.find_elements(by=By.XPATH, value='./li')
         
-        i = 1
-        for item in list_item:
-            print i
-            i += 1
+        list_item[29].click()
+        time.sleep(1)
+        list_item = element.find_elements(by=By.XPATH, value='./li')
+        
+        print 'len : ' + str(len(list_item))
+        for i in range(len(list_item)):
             
-            obj = Item()
-
+            item = list_item[i]
             item.click()
-
-                
-
+            print i
+            obj = Item()
+            
             row0_element = item.find_element_by_xpath('./div[@class=\'p-img\']')
             row0_element = row0_element.find_element_by_xpath('./a')
             obj.item_url = row0_element.get_attribute('href')
             row0_element = row0_element.find_element_by_xpath('./img')
             obj.img_url = row0_element.get_attribute('src')
-            
+             
             row1_element = item.find_element_by_xpath('./div[@class=\'p-name\']')
             obj.title = row1_element.text
+            
+            
+            
             print obj
-            
-            
-            
-#             break
-        break
+        
+#         break
+        print '*' * 90
+
 
 
 
     browser.close()
-
+    print 'over'
 
 
 
 if __name__ == '__main__':
 # jd_search_data = search_data + '&enc=utf-8'
-    crawl('selenium书' + '&enc=utf-8')
+#     crawl('selenium书' + '&enc=utf-8')
 #     crawl('ipad' + '&enc=utf-8')
+    crawl('linux' + '&enc=utf-8')
 
